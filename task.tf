4c26b6d420d069f6cb4e41aea2a24bb7e1420f20
chaithanya045 <104122945+chaithanya045@users.noreply.github.com>
2024-12-30 00:51:49 +0530

    Add files via upload

---
+++ task.tf
@@ -0,0 +1,15 @@
+-----------
+Enter your Name: Ramesh
+Enter starting range: 4
+Enter ending range: 50
+4 is even, Ramesh
+5 is odd, Ramesh
+6 is even, Ramesh
+.
+.
+.
+47 is odd, Ramesh
+48 is even, Ramesh
+49 is odd, Ramesh
+50 is even, Ramesh
+
